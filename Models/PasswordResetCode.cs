a9179f78cdd3f28ff68644cff5c8ed858b9a73ae
Amir-Baddour <amirbaddour675@gmail.com>
2025-06-10 12:22:48 +0300

    create a model called PasswordRestCode

---
+++ Models/PasswordResetCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testing1.Models
+{
+    public class PasswordResetCode
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsUsed { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
